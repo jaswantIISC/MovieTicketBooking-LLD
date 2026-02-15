e96d574db7733853427b16f463bee4b549874415
jaswantIISC <jaswant.meena10@gmail.com>
2026-02-15 21:19:24 +0530

    update Movie class

--- src/entity/Movie.java
+++ src/entity/Movie.java
@@ -5,11 +5,17 @@ public class Movie {
     private final  String id;
     private final String title;
     private final int durationInMinutes;
+    private final String language;
+    private final String genre;
 
-    public Movie(String id, String title, int durationInMinutes) {
+  
+
+    public Movie(String id, String title, int durationInMinutes, String language, String genre) {
         this.id = id;
         this.title = title;
         this.durationInMinutes = durationInMinutes;
+        this.language = language;
+        this.genre = genre;
     }
 
     public String getId() {
@@ -22,6 +28,14 @@ public class Movie {
   
     public int getDurationInMinutes() {
         return durationInMinutes;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getGenre() {
+        return genre;
     }
     
 }
